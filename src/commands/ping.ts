b4852e3c41d4a8a64e95096100bc6fa8f14e9e7c
Zofiii <zofi@xamh.de>
2022-01-05 13:14:59 +0100

    Added required type to ping command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,8 +1,10 @@
+import { ApplicationCommandTypes } from 'discord.js/typings/enums';
 import { Command } from '../lib/Command';
 
 export default new Command({
 	name: 'ping',
 	description: 'replies with pong',
+	type: ApplicationCommandTypes.CHAT_INPUT,
 	run: async ({ interaction }) => {
 		interaction.followUp(`Ping: ${interaction.client.ws.ping}ms`);
 	},
