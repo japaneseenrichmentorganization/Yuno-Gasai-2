78268788f86eb7d1a4d96a4df68e0e307e1cb8e0
Zofiii <zofi@xamh.de>
2022-01-05 13:14:12 +0100

    Refactored command type for better management

--- src/typings/Command.ts
+++ src/typings/Command.ts
@@ -5,6 +5,7 @@ import {
 	GuildMember,
 	PermissionResolvable,
 } from 'discord.js';
+import { ApplicationCommandTypes } from 'discord.js/typings/enums';
 import { ExtendedClient } from './Client';
 
 /**
@@ -16,6 +17,40 @@ import { ExtendedClient } from './Client';
  * }
  * }
  */
+
+
+ type DiscordPermissionString =
+ | 'CREATE_INSTANT_INVITE'
+ | 'KICK_MEMBERS'
+ | 'BAN_MEMBERS'
+ | 'ADMINISTRATOR'
+ | 'MANAGE_CHANNELS'
+ | 'MANAGE_GUILD'
+ | 'ADD_REACTIONS'
+ | 'VIEW_AUDIT_LOG'
+ | 'PRIORITY_SPEAKER'
+ | 'STREAM'
+ | 'VIEW_CHANNEL'
+ | 'SEND_MESSAGES'
+ | 'SEND_TTS_MESSAGES'
+ | 'MANAGE_MESSAGES'
+ | 'EMBED_LINKS'
+ | 'ATTACH_FILES'
+ | 'READ_MESSAGE_HISTORY'
+ | 'MENTION_EVERYONE'
+ | 'USE_EXTERNAL_EMOJIS'
+ | 'VIEW_GUILD_INSIGHTS'
+ | 'CONNECT'
+ | 'SPEAK'
+ | 'MUTE_MEMBERS'
+ | 'DEAFEN_MEMBERS'
+ | 'MOVE_MEMBERS'
+ | 'USE_VAD'
+ | 'CHANGE_NICKNAME'
+ | 'MANAGE_NICKNAMES'
+ | 'MANAGE_ROLES'
+ | 'MANAGE_WEBHOOKS'
+ | 'MANAGE_EMOJIS';
 export interface ExtendedInteraction extends CommandInteraction {
 	member: GuildMember;
 }
@@ -29,6 +64,17 @@ interface RunOptions {
 type RunFunction = (options: RunOptions) => void | Promise<void>;
 
 export type CommandType = {
+	name: string;
+  description: string;
+	type: ApplicationCommandTypes
+	required?: boolean;
 	userPermissions?: PermissionResolvable[];
+  cooldown?: number;
+  guildOnly?: boolean;
+  aliases?: string[];
+  usage?: string;
+  isArgumentsRequired?: boolean;
+  requiredPermissions?: DiscordPermissionString[];
+  requiredRoles?: string[];
 	run: RunFunction;
-} & ChatInputApplicationCommandData;
+}
