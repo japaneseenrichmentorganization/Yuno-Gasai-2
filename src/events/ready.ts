e64f9f9d70572e7a46f0c2fccb9ee3afa94b4d38
Zofiii <zofi@xamh.de>
2022-01-05 12:41:36 +0100

    Added Client<true> param to on ready

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,5 +1,7 @@
-import { Event } from "../lib/Event";
+import { Client } from 'discord.js';
+import { Event } from '../lib/Event';
 
-export default new Event("ready", () => {
-    console.log("Bot is online");
+export default new Event('ready', (client: Client<true>) => {
+	// Do things that should be done when the bot is ready
+	console.log('Bot is online');
 });
