fdd6f32682d747fcd6d7e4392ae4813cc85bee61
Zofiii <zofi@xamh.de>
2022-01-05 18:03:56 +0100

    Added a function to get the settings from the db

--- src/Yuno.ts
+++ src/Yuno.ts
@@ -7,24 +7,28 @@ import {
 import glob from 'glob';
 import { promisify } from 'util';
 
-import { ExtendedClient } from './typings/Client';
+import { ExtendedClient, Settings } from './typings/Client';
 import { Event } from './lib/Event';
 import { CommandType } from './typings/Command';
 import { Connection, IDatabaseDriver, MikroORM } from '@mikro-orm/core';
 import config from './config/mikro-orm.config';
+import { Guilds } from './entities';
 // Used for importing commands and events asyncly
 const globPromise = promisify(glob);
 
 export class Yuno extends Client implements ExtendedClient {
 	public commands: Collection<string, CommandType>;
 	public slashCommands: Array<ApplicationCommandDataResolvable>;
-	public guildID: string | undefined;
-	orm: MikroORM<IDatabaseDriver<Connection>> | undefined;
+	public cooldowns: Collection<string, Collection<string, number>>;
+	public guildID!: string;
+	public settings!: Settings;
+	public orm!: MikroORM<IDatabaseDriver<Connection>>;
 	constructor() {
 		// All intents 32767
 		super({ intents: 32767 });
 		this.commands = new Collection<string, CommandType>();
 		this.slashCommands = new Array<ApplicationCommandDataResolvable>();
+		this.cooldowns = new Collection<string, Collection<string, number>>();
 	}
 
 	async start(token: string, guildID: string) {
@@ -35,8 +39,12 @@ export class Yuno extends Client implements ExtendedClient {
 		this.on('ready', async () => {
 			await this.registerCommands();
 		});
+		// logs the bot in
 		await this.login(token);
+		// inits the orm db object
 		this.orm = await MikroORM.init(config);
+		// parses the settings data from the db into settings property
+		await this._parseSettings();
 	}
 
 	async importFile(filePath: string) {
@@ -77,5 +85,27 @@ export class Yuno extends Client implements ExtendedClient {
 			);
 			this.on(event.event, event.run);
 		});
+	}
+	async _parseSettings() {
+		// Parses the data from the db into the settings type object,
+		// if a value isnt set a default value the for given type is set, e.g false for boolean.
+		// Needs to be called after connecting to the database, get all guilds but we only one so using index 0 is safe
+		const rawSetting = await this.orm.em.getRepository(Guilds).findAll();
+		// Due to the database being setup in a weird way we need to do some cast magic
+		// As the types are TEXT in the database casting like this is safe
+		this.settings = {
+			levelRoleMap:
+				(rawSetting[0].levelRoleMap as unknown as Map<
+					string,
+					string
+				>) ?? new Map<string, string>(),
+			measureXP: (rawSetting[0].measureXP as unknown as boolean) ?? false,
+			spamFilter:
+				(rawSetting[0].spamFilter as unknown as boolean) ?? false,
+			// These followering properties are just string so no casting needed
+			onJoinDMMsg: rawSetting[0].onJoinDMMsg ?? '',
+			onJoinDMMsgTitle: rawSetting[0].onJoinDMMsgTitle ?? '',
+			prefix: rawSetting[0].prefix ?? '',
+		};
 	}
 }
