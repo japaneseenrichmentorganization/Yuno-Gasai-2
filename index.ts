179e3a3da793f3abed89ec6d8cb55e6332f2bacc
11tuvork28 <zofi@xamh.de>
2022-01-15 13:04:32 +0100

    fixed imports and lint

--- index.ts
+++ index.ts
@@ -13,35 +13,36 @@
     along with this program.  If not, see https://www.gnu.org/licenses/.
 */
 import { Yuno } from './src/Yuno';
-import * as DEFAULT_CONFIG from './DEFAULT_CONFIG.json'
-import { BotConfig } from './src/typings/Client';
+import * as DEFAULT_CONFIG from './DEFAULT_CONFIG.json';
+import { BotConfig } from './src/interfaces/Client';
+
 (async () => {
 	try {
-        console.log('Starting Yuno-Gasai-2');
-        // Creates a new Yuno instance, a guild ID must be passed
+		console.log('Starting Yuno-Gasai-2');
+		// Creates a new Yuno instance, a guild ID must be passed
 		const instance = new Yuno();
-		await instance.start(DEFAULT_CONFIG);
-        // Sets a listeners for easier debugging
-        if (process.env.NODE_ENV !== 'production') {
-            process.on('uncaughtException', (err) => {
-                console.log('\x1b[35m', 'Stack-Trace: ' + err.stack);
-            });
-        
-            process.on('unhandledRejection', (err: unknown) => {
-                console.log(
-                    '\x1b[35m',
-                    'Stack-Trace: ' + (err instanceof Error ? err.stack : '')
-                );
-            });
-        }
-        process.on('warning', (warn: Error) =>{
-            console.log('\x1b[35m', warn.name);
-            console.log('\x1b[35m', warn.message);
-            console.log('\x1b[35m', warn.stack);
-            process.exit(1);
-        })
+		await instance.start(DEFAULT_CONFIG as BotConfig);
+		// Sets a listeners for easier debugging
+		if (process.env.NODE_ENV !== 'production') {
+			process.on('uncaughtException', (err) => {
+				console.log('\x1b[35m', 'Stack-Trace: ' + err.stack);
+			});
+
+			process.on('unhandledRejection', (err: unknown) => {
+				console.log(
+					'\x1b[35m',
+					'Stack-Trace: ' + (err instanceof Error ? err.stack : '')
+				);
+			});
+		}
+		process.on('warning', (warn: Error) => {
+			console.log('\x1b[35m', warn.name);
+			console.log('\x1b[35m', warn.message);
+			console.log('\x1b[35m', warn.stack);
+			process.exit(1);
+		});
 	} catch (e: unknown) {
 		//Critical error probably no token and no guildid
-        console.error(`Critical error: ${e instanceof Error ? e.stack : e}`)
+		console.error(`Critical error: ${e instanceof Error ? e.stack : e}`);
 	}
 })();
