5a1a58b69955e6ee2482961b18e8c6bb685f913e
11tuvork28 <zofi@xamh.de>
2022-01-23 22:12:59 +0100

    moved ban function here

--- src/Util.ts
+++ src/Util.ts
@@ -1,4 +1,4 @@
-import { MessageEmbed, TextChannel, User } from 'discord.js';
+import { Message, MessageEmbed, TextChannel, User } from 'discord.js';
 import { Job, scheduleJob, RecurrenceRule, JobCallback } from 'node-schedule';
 import { ExtendedClient } from './interfaces/Client';
 
@@ -53,38 +53,6 @@ async function warnChannel(
 	};
 	channel.send({ embeds: [embed] });
 }
-function formatDuration(seconds: number) {
-	const h = Math.floor(seconds / 3600),
-		min = Math.floor((seconds - h * 3600) / 60),
-		sec = seconds - h * 3600 - min * 60;
-	let r = '';
-
-	if (h > 0) r += ('00' + h).slice(-2) + 'h ';
-
-	if (min > 0) r += ('00' + min).slice(-2) + 'min ';
-
-	if (sec > 0) r += ('00' + sec).slice(-2) + 's';
-
-	return r;
-}
-function checkIfUrl(url: string) {
-	return /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-/]))?/.test(
-		url,
-	);
-}
-function cleanSynopsis(str: string, id: string, type: string) {
-	if (str.length > 2048) {
-		const tmpstr = str.slice(0, 1950).split('.');
-		tmpstr.pop();
-		str = `${tmpstr.join(
-			'.',
-		)}.\n\n[[ Read More ]](https://myanimelist.net/${type}/${id})\n\n`;
-	}
-	return str
-		.replace(/\n\n/g, '\n')
-		.replace(/\[.*\]/g, '')
-		.replace(/\(Source: .*\)/g, '');
-}
 async function getChannelByName(
 	client: ExtendedClient,
 	name: string,
@@ -116,14 +84,27 @@ function produceCleanJob(
 	rule.tz = 'Etc/UTC';
 	return scheduleJob(name + '_clean', rule, callback);
 }
+async function ban(
+	message: Message,
+	id: string,
+	reason: string | undefined,
+): Promise<string> {
+	try {
+		await message.guild?.bans.create(id, {
+			reason: reason ? reason : `Banned by ${message.author.tag}`,
+		});
+		return 'successfully banned.\n';
+	} catch (error) {
+		(message.client as ExtendedClient).emit('error', error as Error);
+		return 'unsuccessfully banned.\n';
+	}
+}
 export default {
 	getAvatarURL,
 	clean,
 	warnChannel,
-	formatDuration,
-	checkIfUrl,
 	produceWarnJob,
-	cleanSynopsis,
 	produceCleanJob,
 	getChannelByName,
+	ban,
 };
